cee83d00f32e16ef457d1177eaf32266920631d2
Raj Madgulkar <rajmadgulkar1007@gmail.com>
2025-03-04 00:14:38 +0530

    Updated on

--- DailyCodes/Array/prog10.java
+++ DailyCodes/Array/prog10.java
@@ -1,9 +1,9 @@
-class TestDemo{
+class T{
 	public static void main(String[]args){
 
-		int arr1[]=new int[]{10,20,30};
+		int arr1[]=new int[]{128,128,128};
 		
-		Integer arr2[]=new Integer[]{10,20,30};
+		Integer arr2[]=new Integer[]{128,128,128};
 
 		System.out.println("The array1 elements are:");
 		for(int i=0;i<arr1.length;i++){
